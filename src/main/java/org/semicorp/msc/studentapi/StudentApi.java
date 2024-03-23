b3a7c432b4e2b5cf76003c8023b6ff753d299937
Seweryn <smicho01@student.bbk.ac.uk>
2024-03-23 13:29:17 +0000

    Update schema names and docker-compose to work along with localdev

--- src/main/java/org/semicorp/msc/studentapi/StudentApi.java
+++ src/main/java/org/semicorp/msc/studentapi/StudentApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentApiApplication {
+public class StudentApi {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudentApiApplication.class, args);
+        SpringApplication.run(StudentApi.class, args);
     }
 
 }
--- src/main/java/org/semicorp/msc/studentapi/domain/student/dao/QueryStudent.java
+++ src/main/java/org/semicorp/msc/studentapi/domain/student/dao/QueryStudent.java
@@ -2,6 +2,6 @@ package org.semicorp.msc.studentapi.domain.student.dao;
 
 public class QueryStudent {
 
-    static final String QUERY_FIND_ALL = "SELECT * FROM studentsschema.student;";
-    static final String QUERY_FIND_BY_ID = "SELECT * FROM studentsschema.student WHERE id = :id";
+    static final String QUERY_FIND_ALL = "SELECT * FROM students.student;";
+    static final String QUERY_FIND_BY_ID = "SELECT * FROM students.student WHERE id = :id";
 }
