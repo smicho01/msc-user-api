26055cad6a535c7c5bc6a5600aeda543c4222872
Seweryn <smicho01@student.bbk.ac.uk>
2024-07-20 18:42:25 +0100

    Update SQL

--- db/sql/001-create-users-table.sql
+++ db/sql/001-create-users-table.sql
@@ -1,6 +1,3 @@
-
--- CREATE SCHEMA IF NOT EXISTS users;
-
 CREATE TABLE IF NOT EXISTS users.user (
     id VARCHAR(36)  PRIMARY KEY,
     username VARCHAR(36) NOT NULL,
@@ -19,13 +16,6 @@ CREATE TABLE IF NOT EXISTS users.user (
     dateupdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
--- -- -- some dummy data
--- INSERT INTO users.user(id, username, visibleusername, firstname, lastname, email,  active ) VALUES
---     ('7cef2a1b-5ef9-4fc6-88fe-b2572f19bf65', 'johndo01', 'HappyUnicorn' ,'John', 'Doe', 'jdoe@gmail.com', true),
---     ('4d9d8092-b95d-4106-866f-96ef2490252b', 'adasmi30', 'SmartRadish' ,'Adam', 'Smith', 'asmith@gmail.com', true),
---     ('f2e43664-2adf-4abd-97ee-3782e98c39e4', 'annflo01', 'PotentialPineapple' ,'Anna', 'Flowers-Kitty', 'aflowers@gmail.com', true);
-
-
 CREATE TABLE users.friends (
     user_id VARCHAR(36) NOT NULL,
     friend_id VARCHAR(36) NOT NULL,
@@ -35,4 +25,3 @@ CREATE TABLE users.friends (
     FOREIGN KEY (user_id) REFERENCES users."user"(id),
     FOREIGN KEY (friend_id) REFERENCES users."user"(id)
 );
-
