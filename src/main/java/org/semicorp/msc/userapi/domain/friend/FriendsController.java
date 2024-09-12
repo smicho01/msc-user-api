16778e91f492e3b8ce4c18b4a27b619f1cd2caba
Seweryn <smicho01@student.bbk.ac.uk>
2024-09-13 00:53:15 +0100

    Add FriendsController tests suite

--- src/main/java/org/semicorp/msc/userapi/domain/friend/FriendsController.java
+++ src/main/java/org/semicorp/msc/userapi/domain/friend/FriendsController.java
@@ -6,6 +6,7 @@ import org.semicorp.msc.userapi.domain.friend.requests.FriendRequestBody;
 import org.semicorp.msc.userapi.domain.friend.response.FriendRequestResponse;
 import org.semicorp.msc.userapi.services.FriendsService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,21 +20,27 @@ public class FriendsController {
 
     private final FriendsService friendsService;
 
-    @GetMapping("{userId}")
+    @GetMapping(value = "{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Friend>> getAllUserFriends(@PathVariable("userId") String userId) {
         List<Friend> allUserFriends = friendsService.getAllUserFriends(userId);
         return new ResponseEntity<>(allUserFriends, HttpStatus.OK);
     }
 
-    @PostMapping("request")
+    @PostMapping(value = "request",  produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Boolean> sendFriendRequest(@RequestBody FriendRequestBody friendRequest) {
         log.info("Friend request endpoint");
-        Boolean result = friendsService.sendFriendRequest(friendRequest.getRequestingUserId(),
-                friendRequest.getRequestedUserId());
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        try {
+            Boolean result = friendsService.sendFriendRequest(friendRequest.getRequestingUserId(),
+                    friendRequest.getRequestedUserId());
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("There was an error while sending a fried request between {} and {}. ERROR: {}" ,
+                    friendRequest.getRequestingUserId(), friendRequest.getRequestedUserId(), e.getMessage() );
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @DeleteMapping("request")
+    @DeleteMapping(value = "request", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Boolean> deleteFriendRequest(@RequestBody FriendRequestBody friendRequest) {
         log.info("Delete friend request endpoint");
         Boolean result = friendsService.deleteFriendRequest(friendRequest.getRequestingUserId(),
@@ -46,7 +53,7 @@ public class FriendsController {
      * @param userId
      * @return
      */
-    @GetMapping("requests/received/{userId}")
+    @GetMapping(value ="requests/received/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<FriendRequestEntity>> getFriendRequestsForUserId(
                             @PathVariable("userId") String userId) {
         log.info("Get received friend requests for user id: {}", userId);
@@ -60,7 +67,7 @@ public class FriendsController {
      * @param userId
      * @return
      */
-    @GetMapping("requests/sent/{userId}")
+    @GetMapping(value = "requests/sent/{userId}",  produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<FriendRequestEntity>> getFriendInvitationsForUserId(@PathVariable("userId") String userId) {
         log.info("Get sent friend requests for user id: {}", userId);
         List<FriendRequestEntity> requests = friendsService.getFriendRequestsForUserId(userId, "pending");
@@ -68,7 +75,7 @@ public class FriendsController {
         return new ResponseEntity<>(requests, HttpStatus.OK);
     }
 
-    @PutMapping("request/accept")
+    @PutMapping(value = "request/accept", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Boolean> acceptFriendRequest(@RequestBody FriendRequestBody friendRequest) {
         log.info("Accept friend request between {} and {}", friendRequest.getRequestedUserId(), friendRequest.getRequestingUserId());
         Boolean result = friendsService.acceptFriendRequest(friendRequest.getRequestingUserId(),
